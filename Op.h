cc9d95e24160dede5e61a61dc1c09139e6409f0b
Justin Chandra <jchan110@ucr.edu>
2017-01-30 18:38:48 -0800

    other skeleton

---
+++ Op.h
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+class Op
+{
+	public:
+	int x;
+		Op(int x)
+		{
+			this->x = x;
+		}
+};
---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+#include "Add.h"
+#include "Base.h"
+#include "Div.h"
+#include "Mult.h"
+#include "Op.h"
+#include "Sqr.h"
+#include "Sub.h"
+
+int main() {
+	Op* op7 = new Op(7);
+	Op* op4 = new Op(4);
+	Op* op3 = new Op(3);
+	Op* op2 = new Op(2);
+	Mult* A = new Mult(op7, op4);
+	Add* B = new Add(op3, A);
+	Sub* C = new Sub(B, op2);
+	Sqr* D = new Sqr(C);
+	VectorContainer* container = new VectorContainer();
+	container->add_element(A);
+	container->add_element(B);
+	container->add_element(C);
+	container->add_element(D);
+	cout << "Container Before Sort: " << endl;
+	container->print();
+	cout << "Container After Sort: " << endl;
+	container->set_sort_function(new SelectionSort());
+	container->sort();
+	container->print();
+};
