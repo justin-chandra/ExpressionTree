f8fd28fea724879121d84a60f12c62c1bae92d83
Audrey Der <ader003@ucr.edu>
2017-01-30 20:20:43 -0800

    VectorContainer progress

--- VectorContainer.cpp
+++ VectorContainer.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <exception>
 #include "Base.h"
 #include "VectorContainer.h"
 
@@ -7,11 +8,52 @@ using namespace std;
 
 void set_sort_function(Sort* sort_function)
 {
-
+	this->sort_function = sort_function; 	
 	return;
 }
 
 virtual void add_element(Base* element)
+{
+
+}
+
+virtual void print()
+{
+	try
+	{
+		if(sort_function == NULL)
+		{
+			throw nullSortException("Sorting Algorithm set to NULL");
+		}
+	}
+
+	catch (const nullSortException& exception)
+	{
+		return;
+	}
+
+	
+
+
+}
+
+virtual void sort()
+{
+
+
+}
+
+virtual void swap(int i, int j)
+{
+
+}
+
+virtual Base* at(int i)
+{
+
+}
+
+virtual int size()
 {
 
 }
