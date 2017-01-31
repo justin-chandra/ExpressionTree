a3ca20225e3e246671b884b258c223beb948505b
Audrey Der <ader003@ucr.edu>
2017-01-30 19:05:31 -0800

    more skeletons

--- Sub.h
+++ Sub.h
@@ -6,20 +6,24 @@ using namespace std;
 class Sub: public Base
 {
 	public:
-		Sub()
-		{
-			
-		}
+		double x;
+		double y; //subtract y from x
 		
+		Sub(Op * a, Op *b)
+		{
+			this->x = x.evaluate();
+			this->y = y.evaluate();
+		}		
+
 		Sub(Base * a, Base * b)
 		{
-		
+			this->x = x.evaluate();
+			this->y = y.evaluate();
 		}
 
 		virtual double evaluate()
-		{
-						
-			return ;
+		{			
+			return x - y;
 		}
 		
 	private:		
