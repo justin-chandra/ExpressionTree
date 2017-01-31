b5d302db600fc8b863a8264d8c72a54dbbaa39c3
Audrey Der <ader003@ucr.edu>
2017-01-30 19:11:09 -0800

    pointers oops Sub.h

--- Sub.h
+++ Sub.h
@@ -11,14 +11,14 @@ class Sub: public Base
 		
 		Sub(Op * a, Op *b)
 		{
-			this->x = x.evaluate();
-			this->y = y.evaluate();
+			this->x = x->evaluate();
+			this->y = y->evaluate();
 		}		
 
 		Sub(Base * a, Base * b)
 		{
-			this->x = x.evaluate();
-			this->y = y.evaluate();
+			this->x = x->evaluate();
+			this->y = y->evaluate();
 		}
 
 		virtual double evaluate()
