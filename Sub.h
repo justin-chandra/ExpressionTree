37770e9af98bd08c8d827d3fd692b0bdf9dcc988
Audrey Der <ader003@ucr.edu>
2017-01-30 18:52:48 -0800

    Audrey's Skeletons

--- Sub.h
+++ Sub.h
@@ -3,9 +3,19 @@
 #include "Op.h"
 using namespace std;
 
-class Subtract: public Base
+class Sub: public Base
 {
 	public:
+		Sub()
+		{
+			
+		}
+		
+		Sub(Base * a, Base * b)
+		{
+		
+		}
+
 		virtual double evaluate()
 		{
 						
