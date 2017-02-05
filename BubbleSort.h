5e146b19c554884f9cf12b705b4c7a5cf03e97aa
Audrey Der <ader003@ucr.edu>
2017-02-04 16:26:57 -0800

    something broke

--- BubbleSort.h
+++ BubbleSort.h
@@ -18,26 +18,26 @@ class BubbleSort: public Sort
 
 		void sort(Container * container)
 		{
-			/*
-			   int flag = 1; //flag is set to 1 to begin first pass
-			   int holder = 0; //holds variable
-			   int size = container->size(); //length of Vector
+			int flag = 1; //flag is set to 1 to begin first pass
+			int holder = 0; //holds variable
+			int size = container->size(); //length of Vector
 
-			   for (int i = 1; (i <= size) && flag; ++i)
-			   {
-			   flag = 0;
-			   for (int j = 0; j < size; ++j)
-			   {
-			   if (container->at(j + 1) > container->at(j))
-			   {
-			//holder = container->at(j);
-			//container->at(j) = container->at(j + 1);
-			//container->at(j + 1) = holder;
-			flag = 1; //swap has occured
+			for (int i = 1; (i <= size) && flag; ++i)
+			{
+				flag = 0;
+			   	for (int j = 0; j < size; ++j)
+			   	{	
+			   		if (container->at(j + 1) > container->at(j))
+			   		{
+						//holder = container->at(j);
+						//container->at(j) = container->at(j + 1);
+						//container->at(j + 1) = holder;
+						swap(j, j + 1);
+						flag = 1; //swap has occured
+					}
+				} 
 			}
-			} 
-			}
-			 */
+		
 		}
 
 };
